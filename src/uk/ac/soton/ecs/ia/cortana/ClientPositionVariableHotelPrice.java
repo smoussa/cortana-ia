58e3eb5549ace820c44425bd07675ec49a3104a7
Charles Sherman <shermzor@gmail.com>
2014-11-20 17:12:09 +0000

    Create preference strategy

---
+++ src/uk/ac/soton/ecs/ia/cortana/ClientPositionVariableHotelPrice.java
@@ -0,0 +1,17 @@
+package uk.ac.soton.ecs.ia.cortana;
+
+import java.util.ArrayList;
+import java.util.Map;
+
+public class ClientPositionVariableHotelPrice extends ClientPosition {
+
+	private Map<HotelAuction, Double> clientHotelPrices;
+	
+	public ClientPositionVariableHotelPrice(ClientPreference client,
+											FlightAuction inFlight, FlightAuction outFlight,
+											Map<HotelAuction, Double> clientHotelPrices) {
+		super(client, inFlight, outFlight, new ArrayList<>(clientHotelPrices.keySet()));
+		this.clientHotelPrices = clientHotelPrices;
+	}
+
+}
